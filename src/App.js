cc759e5e6f4ad4a75f2a25b27ef1d234a0f45267
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2022-02-09 14:43:55 +0200

    create bootstrab form and add bbotstrab cdn to index.html file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,61 @@
+import formJSON from "./formElement.json";
+
 function App() {
-  return <div className="App">dinamic form</div>;
+  return (
+    <>
+      {JSON.stringify(formJSON)}
+      <div className="App container mt-5 ">
+        <div className="col-md-6">
+          <form>
+            <div className="form-group">
+              <label htmlFor="exampleInputEmail1">Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+              />
+              <small id="emailHelp" className="form-text text-muted">
+                We'll never share your email with anyone else.
+              </small>
+            </div>
+            <div className="form-group">
+              <label htmlFor="exampleInputPassword1">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                id="exampleInputPassword1"
+                placeholder="Password"
+              />
+            </div>
+            <select
+              className="form-select mt-3"
+              aria-label="Default select example"
+            >
+              <option>Open this select menu</option>
+              <option>One</option>
+              <option>Two</option>
+              <option>Three</option>
+            </select>
+            <div className="form-check ">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="exampleCheck1"
+              />
+              <label className="form-check-label" htmlFor="exampleCheck1">
+                Check me out
+              </label>
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
